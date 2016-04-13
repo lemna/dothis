f6fe20247178d9d13b6a5560fbeb02745beb0f04
Peter Konings <peter.l.e.konings@gmail.com>
2016-04-14 00:41:45 +0200

    added functionality to generate lists for all R and Rmd files in the active project

--- R/generate_todolist.R
+++ R/generate_todolist.R
@@ -1,27 +1,57 @@
 #' Generate a "To Do" List in the Rstudio markers pane
 #'
-#' This function looks for comments in the active document starting with the
+#' This function looks for comments in a file starting with the
 #' words 'todo' or 'fixme' and gathers them in the markers pane. This function
 #' should never be called directly - it is added as an item in the Addins menu.
+#'
+#' @param filename optional filename
 #'
 #' @return returns invisibly
 #' @import rstudioapi stringr
 #' @export
 generate_todolistAddin <- function(){
-  context <- rstudioapi::getActiveDocumentContext()
-  print(context$path)
+  generate_todolist()
+}
+
+#' @rdname generate_todolistAddin
+generate_todolist_projectAddin <- function(){
+  rstudioapi::verifyAvailable()
+  project <- rstudioapi::getActiveProject()
+  filenames <- list.files(path = project,
+                          pattern = "(.R$)|(.Rmd$)",
+                          include.dirs = TRUE,
+                          recursive = TRUE)
+  lapply(filenames, generate_todolist)
+  return(invisible())
+}
+
+#' @rdname generate_todolistAddin
+generate_todolist <- function(filename = NULL){
+  rstudioapi::verifyAvailable()
+  if(is.null(filename)){
+    context <- rstudioapi::getActiveDocumentContext()
+    filename <- context$path
+    if(filename == '' ) stop(call. = FALSE, 'Save file first')
+    contents <- context$contents
+  } else {
+    contents <- readLines(filename)
+  }
+
   todo_list_keywords <- "(?is)(#+'* *todo:?)|(#+'* *fixme:?)"
-  todolist <- str_subset(string = context$contents,
+
+  basePath <- rstudioapi::getActiveProject()
+
+  todolist <- str_subset(string = contents,
                          pattern = todo_list_keywords) %>%
     str_replace(pattern = todo_list_keywords,
                 replacement = "") %>%
     str_trim(side = "both")
-  locations <- which(str_detect(string = context$contents,
+  locations <- which(str_detect(string = contents,
                                 pattern = todo_list_keywords))
   markers <- lapply(seq_along(todolist), function(x){
     marker <- list()
     marker$type <- "usage"
-    marker$file <- context$path
+    marker$file <- filename
     marker$line <- locations[x]
     marker$column <- 1
     marker$message <- todolist[x]
@@ -29,8 +59,9 @@ generate_todolistAddin <- function(){
   })
 
   rstudioapi::callFun("sourceMarkers",
-                      name = "To Do List",
-                      markers = markers)
+                      name = paste("To Do List for", filename),
+                      markers = markers,
+                      basePath = basePath)
   return(invisible())
 }
 
