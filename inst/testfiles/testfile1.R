b6574481eb196d4cde1acca831d7573d673ff48d
Peter Konings <peter.l.e.konings@gmail.com>
2016-04-17 00:44:08 +0200

    added testfiles

---
+++ inst/testfiles/testfile1.R
@@ -0,0 +1,3 @@
+# This is a test file.
+# TODO: this is line 2.
+# fixme this is line 3.
---
+++ inst/testfiles/testfile2.R
@@ -0,0 +1,5 @@
+# This is another testfile. This one contains roxygen comments.
+#' fixme: add @author. this is line 2.
+#' TOdo: this is line 3 of the roxygen stuff.
+#' this is line 4. It should not show up in the list, because it does not start with fixme or todo.
+#'
