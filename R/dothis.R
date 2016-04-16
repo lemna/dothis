0aa143e91eab79d48c641a7e1197bddf76069e48
Peter Konings <peter.l.e.konings@gmail.com>
2016-04-17 00:26:08 +0200

    version bump; added package documentation file

---
+++ R/dothis.R
@@ -0,0 +1,11 @@
+#' dothis: a very basic "To Do" List for RStudio
+#'
+#' This package adds 2 items to the Addins menu in RStudio and provides a very
+#' basic "To Do" List, inspired by the task pane in Eclipse.
+#'
+#' All comments starting with the keywords "todo" or "fixme" will be listed in
+#' the Markers pane.
+
+#' @docType package
+#' @name dothis
+NULL
